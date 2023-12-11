0f275281e530d7c3aeba5feea69fc191f1f1d416
Jose Garcia <garciacolin@hotmail.com>
2023-12-11 16:59:51 -0600

    Creating Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'
+import './App.css';
+import Header from './components/Header.jsx';
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className='app'>
+      <Header />
+    </div>
   )
 }
 
